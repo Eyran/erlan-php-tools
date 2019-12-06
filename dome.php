196feac2cabd1023deacbd2ddbd250a8ad45ff99
Eyran <xw-xcp@139.com>
2019-12-06 16:29:49 +0800

    1.提交composer测试库

---
+++ dome.php
@@ -0,0 +1,9 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Tools\Algorithm\Base;
+
+$base = new Base();
+
+$base->index();
---
+++ src/Tools/Algorithm/Base.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tools\Algorithm;
+
+class Base
+{
+    public function index()
+    {
+        echo "欢迎使用";
+    }
+}
